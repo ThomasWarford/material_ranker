5a12448150cc4f41b2140ea84832bc8e38ba4932
Thomas <thomasdwarford@hotmail.co.uk>
2024-02-27 17:36:49 +0000

    Add retrieval of paper titles and whether materials have been previously synthesized

--- Ranker.py
+++ Ranker.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+from mp_api.client import MPRester
+from key import API_KEY # keep API key off github!
+import re
 
 
 class Ranker():
@@ -13,14 +16,48 @@ class Ranker():
         prompt = f"{self.initial_prompt}\n{df.to_csv()}"
 
     def create_table(self, material_ids):
-        df = pd.DataFrame(index=material_ids, columns=["synthesizability"])
+        df = pd.DataFrame(index=material_ids, columns=["experimentally_observed", "papers"])
         df.index.name = "material_id"
-        
+
         for material_id in material_ids:
-            df.loc[material_id, "synthesizability"] = self.get_synthesizability(material_id)
+            df.loc[material_id, "experimentally_observed"] = self.get_experimentally_observed(material_id)
+            df.loc[material_id, "papers"] = ";".join(self.get_paper_titles(material_id))
             
         return df
     
-    def get_synthesizability(self, material_id):
-        '''TODO implement'''
-        return 0.5
+
+    def get_experimentally_observed(self, material_id: str):
+        with MPRester(API_KEY) as mpr:
+            data = mpr.summary.search(material_ids=material_id, fields=["theoretical"])
+
+        return not data[0].theoretical
+
+
+    def get_paper_titles(self, material_id: str):
+        with MPRester(API_KEY) as mpr:
+            bibtexs = mpr.get_material_id_references(material_id)
+
+        pattern = r'title\s*=\s*"([^"]+)"'
+
+        titles = []
+
+        for bibtex in bibtexs: 
+
+            match = re.search(pattern, bibtex)
+            if match:
+                titles.append(match.group(1))
+
+        return titles
+    
+    def get_theoretical_list(self, material_ids: list):
+        '''Unused and untested'''
+        with MPRester(API_KEY) as mpr:
+            data = mpr.summary.search(material_ids=material_ids, fields=["item.material_id", "theoretical"])
+
+        is_theoretical = {}
+
+        for item in data:
+            is_theoretical[item.material_id](item.theoretical)
+
+        return is_theoretical
+
--- tests/test_ranker.py
+++ tests/test_ranker.py
@@ -2,11 +2,22 @@ from Ranker import Ranker
 
 
 ranker = Ranker("model", "You are an AI")
-df = ranker.create_table(["mp-10", "mp-100", "mp-1000"])
 
-## Test table creation
+
+def test_api_requests():
+    mat_1 = "mp-10101"
+    mat_2 = "mp-1245205"
+
+    assert ranker.get_experimentally_observed(mat_1) == True
+    assert ranker.get_experimentally_observed(mat_2) == False
+
+    expected_paper_titles_1 = ['Novel ternary alkali metal silver acetylides M(1) Ag C2 (M(1)= Li, Na, K, Rb, Cs)']
+    assert ranker.get_paper_titles(mat_1) == expected_paper_titles_1
+
+    expected_paper_titles_2 = ['Thermodynamic limit for synthesis of metastable inorganic materials']
+    assert ranker.get_paper_titles(mat_2) == expected_paper_titles_2
+
 def test_table():
-    # check for NaNs
-    assert df.isna().any(axis=None) == False
-    # check table dimensions
-    assert df.shape == (3, 1)
+    df = ranker.create_table(["mp-10101", "mp-1245205"])
+    print(df)
+
