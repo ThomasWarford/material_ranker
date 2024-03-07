3340b240465756c81c47951f910de23b617432bd
thomas warford <thomas.warford0@gmail.com>
2024-03-07 13:50:37 +0000

    Generating dataframe of metrics

--- src/get_from_mp_api.py
+++ src/get_from_mp_api.py
@@ -6,7 +6,7 @@ import pandas as pd
 from mp_api.client import MPRester
 from key import API_KEY # keep API key off github!
 
-def get_titles_from_mp(mp_ids)
+def get_titles(material_ids: list)
     with MPRester(API_KEY) as mpr:
         docs = mpr.materials.provenance.search(fields=["material_id", "references"], material_ids=material_id)
 
@@ -33,3 +33,40 @@ def get_titles_from_mp(mp_ids)
         title_ignored_dict[material.material_id] = titles_ignored
 
     return title_dict, title_ignored_dict
+
+def get_experimentally_observed(material_ids: list):
+    with MPRester(API_KEY) as mpr:
+        data = mpr.materials.summary.search(material_ids=material_ids, fields=["material_id", "theoretical"])
+    
+    output = {}
+    for material in data:
+        output[material.material_id] = not material.theoretical
+    
+    return output
+    
+def get_band_gap(material_id: list):
+    with MPRester(API_KEY) as mpr:
+        data = mpr.materials.summary.search(material_ids=material_ids, fields=["material_id", "band_gap"])
+    
+    band_gaps = {}
+    for material in data:
+        data[material.material_id] = material.band_gap
+    
+    return band_gaps
+
+def create_table(material_ids):
+        #col_names = ["experimentally_observed", "papers"]
+        col_names = ["experimentally_observed", "band_gap"]
+        
+        df = pd.DataFrame(index=material_ids, columns=col_names)
+        df.index.name = "material_id"
+
+        for material_id in material_ids:
+            df.loc[material_id, "experimentally_observed"] = self.get_experimentally_observed(material_id)
+            df.loc[material_id, "band_gap"] = self.get_band_gap(material_id)
+            #df.loc[material_id, "papers"] = ";".join(self.get_paper_titles(material_id))
+            
+        return df
+    
+
+
